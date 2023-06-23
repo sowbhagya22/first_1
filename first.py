f403ee7ea33fb701795e29a69534acfc51f4e505
sowbhagya22 <137481332+sowbhagya22@users.noreply.github.com>
2023-06-23 12:58:00 +0530

    Update first.py

--- first.py
+++ first.py
@@ -5,3 +5,8 @@ streamlit.text(' 🥣 omega 3 & blueberry oatmeal')
 streamlit.text(' 🥗 Kale, spinach & rocket smoothie')
 streamlit.text('🐔 hard-boiled free-range egg')
 streamlit.text('🥑🍞 Avocado toast')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
