eb8c610a0f1d7f2b0c17f45e2180af0ce099681f
sowbhagya22 <137481332+sowbhagya22@users.noreply.github.com>
2023-06-23 12:42:34 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('my parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & blueberry oatmeal')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('hard-boiled free-range egg')
+streamlit.text(' 🥣 omega 3 & blueberry oatmeal')
+streamlit.text(' 🥗 Kale, spinach & rocket smoothie')
+streamlit.text('🐔 hard-boiled free-range egg')
+streamlit.text('🥑🍞 Avocado toast')
